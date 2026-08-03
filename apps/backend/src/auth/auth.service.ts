8e3ff38a9d5100e6c20877a4ce3e15417f859771
Mohd Jibraeel <mohdjibrail424@gmail.com>
2026-08-03 14:17:23 +0530

    feat: implement /auth/login with JWT access and refresh tokens

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, ConflictException } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { Injectable, ConflictException,UnauthorizedException} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt/dist/jwt.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(dto: RegisterDto) {
     const existingUser = await this.prisma.user.findUnique({
@@ -30,5 +33,34 @@ export class AuthService {
       email: user.email,
       role: user.role,
     };
+  }
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordMatches = await bcrypt.compare(dto.password, user.password);
+
+    if (!passwordMatches) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user.id, email: user.email, role: user.role };
+
+    const accessToken = await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_ACCESS_SECRET,
+      expiresIn: '15m',
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_REFRESH_SECRET,
+      expiresIn: '7d',
+    });
+
+    return { accessToken, refreshToken };
   }
 }
